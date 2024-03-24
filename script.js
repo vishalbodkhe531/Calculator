69b5bcb6d1a974e465e14c0e80d36a8fe9ef80de
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2024-03-24 17:55:02 +0530

    Add in gitHub

---
+++ script.js
@@ -0,0 +1,27 @@
+const btns = document.querySelectorAll(".button");
+const input = document.querySelector("#input");
+let str = '';
+Array.from(btns).forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        const firstValue = input.value[0] 
+        if (btn.innerHTML === '=') {
+            input.value = eval(input.value);
+        }
+         else if (btn.innerHTML === 'AC') {
+            str = ''
+            input.value = str;
+        } 
+        else if (btn.innerHTML === 'del') {
+        str = str.substring(0,str.length-1)
+        input.value = str;
+        } else if((firstValue == ('*')) || (firstValue == ('-')) || (firstValue == ('+')) || (firstValue == ('/')) || (firstValue == ('%'))){
+            str = 'error'
+            input.value = str;
+        }
+        else {
+            str += btn.innerHTML
+            input.value = '';
+            input.value = str;
+        }
+    });
+});
