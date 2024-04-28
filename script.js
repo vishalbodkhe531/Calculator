93ba435ba776fce28cb43fa28661f82dd8508c61
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2024-04-28 09:55:52 +0530

    Add voice

--- script.js
+++ script.js
@@ -4,43 +4,51 @@ const input = document.querySelector("#input");
 
 let speaker = new SpeechSynthesisUtterance();
 
-let str = '';
+let str = "";
+
+const voiceGenerat = (voice) => {
+  speaker.text = voice;
+  window.speechSynthesis.speak(speaker);
+};
 
 Array.from(btns).forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const firstValue = input.value[0];
 
-    btn.addEventListener("click", (e) => {
-
-        const firstValue = input.value[0] 
-
-        if (btn.innerHTML === '=') {
-            input.value = eval(input.value);
-        }
-
-         else if (btn.innerHTML === 'AC') {
-            str = ''
-            input.value = str;
-        } 
-            
-        else if (btn.innerHTML === 'del') {
-
-            str = str.substring(0, str.length - 1)
-            
-            input.value = str;
-            
-        }
-        
-        else if ((firstValue == ('*')) || (firstValue == ('-')) || (firstValue == ('+')) || (firstValue == ('/')) || (firstValue == ('%'))) {
-            str = 'error'
-            input.value = str;
-        }
-            
-        else {
-            str += btn.innerHTML
-            input.value = '';
-            input.value = str;
-              
-        }
-        speaker.text = input.value;
-        window.speechSynthesis.speak(speaker);
-    });
+    if (btn.innerHTML === "=") {
+      input.value = eval(input.value);
+      voiceGenerat(`Answer is ${input.value}`);
+    }
+    
+    else if (btn.innerHTML === "AC") {
+      str = "";
+        input.value = str;
+        voiceGenerat(`All clear`);
+    }
+    
+    else if (btn.innerHTML === "del") {
+      str = str.substring(0, str.length - 1);
+        input.value = str;
+          voiceGenerat(`delete`);
+    }
+    
+    else if (
+      firstValue == "*" ||
+      firstValue == "-" ||
+      firstValue == "+" ||
+      firstValue == "/" ||
+      firstValue == "%"
+    ) {
+      str = "error";
+        input.value = str;
+        voiceGenerat(`Please give me number`);
+    }
+    
+    else {
+      str += btn.innerHTML;
+      input.value = "";
+      input.value = str;
+      voiceGenerat(btn.innerHTML);
+    }
+  });
 });
